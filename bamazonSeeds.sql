21fe7b7d5a7cdaf62ed3ed1cd997358ddc1516b3
Jeremy Gill <jeremygill87@gmail.com>
2019-12-03 10:05:59 -0500

    Set up files and set up basic framework for database and products table

---
+++ bamazonSeeds.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products (
+    item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(50) NOT NULL,
+    price FLOAT default 0;
+    stock_quantity INT default 0;
+    PRIMARY KEY (item_id);
+)
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+    ("Fossilized Kidney Stones", "Oddities", 16.00, 50),
+    ("The Communist Mani-Pedi", "Books", 24.00, 25),
+    ("Ben Ng's Knowledge", "Impossibilities", 5000.00, 10),
+    ("Your Father's Approval", "Impossibilities", 15.95, 30),
+    ("Super Mario Batali", "Video Games", 39.95, 50),
+    ("Jesus Christ Vampire Hunter", "DVDs", 19.99, 20),
+    ("Miniature Equine Accoutrements", "Fashion", 29.99, 100),
+    ("Happiness That Isn't Fleeting", "Impossibilities", 50.00, 250),
+    ("")
+
