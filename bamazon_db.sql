267f9df99b06de668bd3b7bc7d7196132be6335e
Jeremy Gill <jeremygill87@gmail.com>
2019-12-07 09:37:57 -0500

    Small text changes

--- bamazon_db.sql
+++ bamazon_db.sql
@@ -7,10 +7,10 @@ CREATE TABLE products (
     item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(100) NOT NULL,
     department_name VARCHAR(50) NOT NULL,
-    price FLOAT default 0;
-    stock_quantity INT default 0;
-    PRIMARY KEY (item_id);
-)
+    price FLOAT default 0,
+    stock_quantity INT default 0,
+    PRIMARY KEY (item_id)
+);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES 
